c76f00e0d47258921c1df224ada07a54e2161135
RobbertBons <74967915+RobbertBons@users.noreply.github.com>
2024-01-27 21:29:04 +0100

    header begin

--- main/index.php
+++ main/index.php
@@ -5,8 +5,11 @@ require_once 'partial/header.php';
     <link rel="stylesheet" type="text/css"  href="css/main.css">
 
     <section class="banner">
-        <article>
-            <h1>Paleo Chronicle</h1>
+        <article >
+            <h1>Welcome by Paleo Chronicle</h1>
+        </article>
+        <article class="species">
+            <p>Species<p>
         </article>
     </section>
 
---
+++ main/partial/header.php
@@ -0,0 +1,14 @@
+
+<link rel="stylesheet" type="text/css"  href="css/header.css">
+
+<section>
+    <article>
+        <ul>
+            <a href="">Home</a>
+            <a href="">Continents</a>
+            <a href="">About</a>
+            <a href="" class="button">Login</a>
+            <a href="" class="button">Register</a>
+        </ul>
+    </article>
+</section>
