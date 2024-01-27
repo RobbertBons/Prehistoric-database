db43aa68e69db4eda3638e9a52b8accc138af5a5
RobbertBons <74967915+RobbertBons@users.noreply.github.com>
2024-01-27 16:22:27 +0100

    een titel

--- main/index.php
+++ main/index.php
@@ -1,3 +1,11 @@
 <?php
-
+require_once 'partial/header.php'
+?>
+<section class="banner">
+    <article>
+        <h1>Paleo Chronicle</h1>
+    </article>
+</section>
+<?php
+require_once 'partial/footer.php'
 ?>
