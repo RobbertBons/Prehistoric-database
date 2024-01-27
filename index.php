f9c33d412d1dfbd4329473132dbe9296fd0e48b1
RobbertBons <74967915+RobbertBons@users.noreply.github.com>
2024-01-28 00:09:18 +0100

    recoverie 2

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
-require_once 'header.php'; 
+require_once 'partials/header.php'
 ?>
 <link rel="stylesheet" type="text/css" href="css/main.css">
-
+<body>
+    <section class = "banner">
+        <article>
+            <h1>Welcome by Paleo Chronical</h1>
+        </article>
+    </section>
+    <section>
+        <article class = 'species'>
+            <p>species</p>
+        </article>
+    </section>
+</body>
 <?php
-require_once 'footer.php';
+require_once 'partials/footer.php';
 ?>
---
+++ partials/header.php
@@ -0,0 +1,8 @@
+<ul>
+    <a href="">Home</a>
+    <a href="">About</a>
+    <a href="">Continets</a>
+    <a href="">My account</a>
+    <a href="" class = 'button'>login</a>
+    <a href="" class = 'button'>registratie</a>
+</ul>
