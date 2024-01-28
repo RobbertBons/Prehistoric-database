bcd231f6c8fcc8163343665d9e6f400f498a1350
Maitri-6018726 <6018726@mborijnland.nl>
2024-01-29 00:27:54 +0100

    heel crud systeem items werkt

---
+++ items/show.php
@@ -0,0 +1,41 @@
+<?php
+// session_start();
+// if( $_SESSION['ingelogd'] != true){
+//     header("Location: ../login.php");
+        //exit();
+// }
+if(isset($_POST['delete'])){
+    require_once ("../classes/dbconnect.php");
+    require_once ("../classes/items.php");
+    $item = new Item();
+    $item->deleteItem($_POST['delete']);
+    header("location: show.php");
+    exit();
+}
+require_once ("../classes/dbconnect.php");
+require_once ("../classes/items.php");
+    $item = new Item();
+    $items = $item->showItem($_POST);
+?>
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
+        <title>Alle Locaties</title>
+    </head>
+    <body>
+        <?php
+        foreach($items as $item){
+            echo $item->naam. " " .$item->beschrijving. " ". $item->diet. " ". $item->lengte. " ". $item->geleefd. " " ."<a href='edit.php?id=".$item->id."'>Bewerk</a> 
+            <form method='POST'>
+            <button name='delete' type='submit' value=$item->id> Verwijderen</button>
+            </form><br>";
+        }
+        ?>
+    </body>
+</html>
