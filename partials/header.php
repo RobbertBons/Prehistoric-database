e9d8348e51a00965b8dcdb2929a7cb285dcbb712
RobbertBons <74967915+RobbertBons@users.noreply.github.com>
2024-03-09 09:34:53 +0100

    style van header

--- partials/header.php
+++ partials/header.php
@@ -1,8 +1,15 @@
-<ul>
-    <a href="index.php">Home</a>
-    <a href="">About</a>
-    <a href="">Continets</a>
-    <a href="">My account</a>
-    <a href="login.php" class = 'button'>login</a>
-    <a href="registratie.php" class = 'button'>registratie</a>
-</ul>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/header.css">
+
+<header class="header">
+    <a href="#" class="logo">Logo</a>
+        <ul class ="nav">
+            <a href="index.php">Home</a>
+            <a href="">About</a>
+            <a href="">Continets</a>
+            <a href="login.php" class = 'button'>Login</a>
+            <a href="">My account</a>
+        </ul>
+</header>
