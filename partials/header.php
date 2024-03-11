ca569dabf2f00b3795d6430c669c2b97de415b7e
RobbertBons <74967915+RobbertBons@users.noreply.github.com>
2024-03-11 12:48:42 +0100

    nieuwe style verbeterd

--- partials/header.php
+++ partials/header.php
@@ -1,15 +1,73 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="css/header.css">
 
 <header class="header">
-    <a href="#" class="logo">Logo</a>
+    <img src="img/image2.png" class="logo" alt="">
         <ul class ="nav">
             <a href="index.php">Home</a>
             <a href="">About</a>
             <a href="">Continets</a>
-            <a href="login.php" class = 'button'>Login</a>
-            <a href="">My account</a>
+            <a href="">Species</a>
         </ul>
+        <a href="login.php" class = 'button'>Login</a>
+        <input type="text" class="search-bar" placeholder="Search">
 </header>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+}
+
+.button{
+    border-radius: 10;
+}
+
+.logo{
+    width: 15%;
+    height: auto;
+    text-decoration: none;
+    color: 	#808080;
+    font-size: 18px;
+    font-weight: 500;
+    margin-right: 190px;
+}
+.search-bar {
+    padding: 4px 5px; /* Adjust padding as needed */
+    border-radius: 5px; /* Add some border-radius for rounded corners */
+    border: 1px solid #ccc; /* Add a border for better visibility */
+}
+
+.header{
+        top: 0;
+        left: 0;
+        width: 100%;
+        padding: 20px 100px;
+        background-color: #fed684;
+        display: flex;
+        align-items: center; /* Vertically center the content */
+        justify-content: space-between;
+}
+
+.nav {
+        list-style: none; /* Remove default list styles */
+        display: flex;
+        align-items: center; /* Vertically center the content */
+    }
+
+.nav li {
+        margin-right: 20px; /* Add spacing between nav items */
+    }
+    
+    .nav a {
+        text-decoration: none;
+        color: 	#808080;
+        font-size: 18px;
+        font-weight: 500;
+        margin-right: 20px;
+    }
+
+</style>
