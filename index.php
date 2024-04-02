ef4b9fbd7e994cc2cc3d4141ef5c4aeee47856fc
RobbertBons <74967915+RobbertBons@users.noreply.github.com>
2024-04-02 14:54:01 +0200

    beetje css

--- index.php
+++ index.php
@@ -24,16 +24,18 @@
     </article>
 </section>
 
-<div class="grid-container">
+<div class="container">
+    <div class="grid-container">
         <article class='forum'>
             <h1>Forum</h1>
             <p>Join our community and discussions</p>
         </article>
 
-        <article class='latest news'>
+        <article class='latestnews'>
             <h1>Latest news</h1>
             <p>Our newest Paleo information here</p>
         </article>
+    </div>
 </div>
 
 </body>
--- registratie.php
+++ registratie.php
@@ -13,13 +13,15 @@ if(isset($_POST['register'])){
     <main>
     	<section class="form">
 	    	<form method="post">
-	    		<label for="username" id="username">Gebruikersnaam: </label>
+	    		<label for="username" id="username">Username: </label>
 	    		<input type="text" name="username" required>
-        		voornaam:<input type="text" name="firstname">
-				achternaam:<input type="text" name="lastname">
-	    		<label for="password">Wachtwoord: </label>
+				<label for="firstname" id="username">Name:</label>
+        		<input type="text" name="firstname" required>
+				<label for="lastname" id="lastname">Lastname:</label>
+				<input type="text" name="lastname" required>
+	    		<label for="password">Password: </label>
 	    		<input type="password" name="password" required>
-	    		<label for="conf-password">Wachtwoord bevestiging: </label>
+	    		<label for="conf-password">Password Confirm: </label>
 	    		<input type="password" name="conf-password" required>
 	    		<input type="submit" name="register" value="Register">
 	    	</form>
